5f139cbcd49fafa29d7a3672374b797f7430c293
Oscar Norman <oscar.norman95@gmail.com>
2022-11-21 01:09:08 +0100

    Removed ascii tables and also removed array within the json that gets returned from reddit

--- bot.py
+++ bot.py
@@ -1,7 +1,6 @@
 # bot.py
 import discord, json
 from reddit import redditBot
-from table2ascii import table2ascii as t2a, PresetStyle
 
 TOKEN = 'MTA0MzkxMjMzNDIwNjMxNjU5NQ.GO3C8F.AcZRbnoL5A69iIeFtRyVZFmAJ1sOxu5VwOy5HM'
 CHANNEL_ID = '1021484821802930256'
@@ -22,25 +21,20 @@ async def on_message(message):
         if (str(message.author) != str(client.user)):
             channel = client.get_channel(int(CHANNEL_ID))
 
-            reddit_data = json.loads(redditBot(message.content.split('!')[1]))
+            reddit_raw_data = redditBot(message.content.split('!')[1])
+            reddit_data = json.loads(reddit_raw_data)
 
-            response = ''
-            response += 'TITLE:\n{}'.format(reddit_data['title'])
-            response += '\nUPVOTES: {}'.format(reddit_data['ups'])
+            response = '```'
+            response += '**Title:**\n\n{}'.format(reddit_data['title'])
+            response += '\nUptvotes: {}'.format(reddit_data['ups'])
+            response += '\n\n---TOP COMMENTS---\n\n'
+            response += '#1 Upvotes: {} | {}\n\n'.format(reddit_data['oneUps'], reddit_data['one'])
+            response += '#2 Upvotes: {} | {}\n\n'.format(reddit_data['twoUps'], reddit_data['two'])
+            response += '#3 Upvotes: {} | {}\n\n'.format(reddit_data['threeUps'], reddit_data['three'])
+            response += '#4 Upvotes: {} | {}\n\n'.format(reddit_data['fourUps'], reddit_data['four'])
+            response += '#5 Upvotes: {} | {}\n\n'.format(reddit_data['fiveUps'], reddit_data['five'])
+            response += '```'
             
-            # In your command:
-            output = t2a(
-            header=["# Upvotes", "Comment"],
-            body=[
-            [reddit_data['top_comments'][0]['oneUps'], reddit_data['top_comments'][0]['one']], 
-            [reddit_data['top_comments'][0]['twoUps'], reddit_data['top_comments'][0]['two']], 
-            [reddit_data['top_comments'][0]['threeUps'], reddit_data['top_comments'][0]['three']],
-            [reddit_data['top_comments'][0]['fourUps'], reddit_data['top_comments'][0]['four']],
-            [reddit_data['top_comments'][0]['fiveUps'], reddit_data['top_comments'][0]['five']],
-            ],
-            style=PresetStyle.thin_compact
-            )
+            await channel.send(response)
 
-            await channel.send(output)
-            #await channel.send(response)
 client.run(TOKEN)
--- reddit.py
+++ reddit.py
@@ -12,42 +12,48 @@ def redditBot(subreddit):
     print(reddit.user.me())
 
     subreddit = reddit.subreddit(subreddit)
-
-    hot_AskReddit = subreddit.hot(limit=1)
+    print("SUBREDDIT: {}".format(subreddit))
+    subreddit_hot = subreddit.hot(limit=10)
+    print("17")
 
     all_comments = []
     title = ''
     ups = ''
-    for submission in hot_AskReddit:
-        if not submission.stickied:
-            print('Title: {}, Ups: {}, Downs: {}'.format(submission.title, submission.ups, submission.downs))
-            title = submission.title
-            ups = submission.ups
 
-            submission_comments = submission.comments.list()
-            real_comments = [comment for comment in submission_comments if isinstance(comment, praw.models.Comment)]
-            all_comments += real_comments
+    bestSubmission = ''
+
+    for bestSubmission in subreddit_hot:
+        if not bestSubmission.stickied:
+            bestSubmission = bestSubmission
+            break
+
+    #print('Title: {}, Ups: {}, Downs: {}'.format(submission.title, submission.ups, submission.downs))
+    title = bestSubmission.title
+    ups = bestSubmission.ups
+
+    submission_comments = bestSubmission.comments.list()
+    print('submission_comments size: {}'.format(len(submission_comments)))
+    real_comments = [comment for comment in submission_comments if isinstance(comment, praw.models.Comment)]
+    all_comments += real_comments
+    print(len(all_comments))
 
     all_comments.sort(key=lambda comment: comment.score, reverse=True)
     top_comments = all_comments[:5]
+    print(len(all_comments))
 
     values = {
         'title': title,
         'ups': ups,
-        'top_comments': [
-            {
-                'one': str(top_comments[0].body),
-                'oneUps': str(top_comments[0].score),
-                'two': str(top_comments[1].body),
-                'twoUps': str(top_comments[1].score),
-                'three': str(top_comments[2].body),
-                'threeUps': str(top_comments[2].score),
-                'four': str(top_comments[3].body),
-                'fourUps': str(top_comments[3].score),
-                'five': str(top_comments[4].body),
-                'fiveUps': str(top_comments[4].score)
-            }
-        ]
+        'one': str(top_comments[0].body),
+        'oneUps': str(top_comments[0].score),
+        'two': str(top_comments[1].body),
+        'twoUps': str(top_comments[1].score),
+        'three': str(top_comments[2].body),
+        'threeUps': str(top_comments[2].score),
+        'four': str(top_comments[3].body),
+        'fourUps': str(top_comments[3].score),
+        'five': str(top_comments[4].body),
+        'fiveUps': str(top_comments[4].score)
     }
 
 #    for top in top_comments:
